c89b0261f09ec8937d90a3e5f4826279fe59859b
VNavya <VNavya@NAVYA>
2026-03-26 16:15:33 +0530

    latest code

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173,
-    allowedHosts: 'all'
-  }
+  plugins: [react()]
 })
